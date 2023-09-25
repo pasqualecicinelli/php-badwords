26855231bbd4cedb0a40751887dc573b49a0248a
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-09-25 16:52:10 +0200

    feat Exercise

--- result.php
+++ result.php
@@ -1,6 +1,8 @@
 <?php 
 
-
+$paragrafo = $_GET["paragrafo"];
+$censura = $_GET["censura"];
+$sostituisci = "***";
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +13,20 @@
     <title>PHP Badwords</title>
 </head>
 <body>
+
+<h1>
+
+<?php echo $intero_par = $paragrafo . " " . $censura . " ";
+
+echo strlen($intero_par) . "<br>";
+
+echo $new_paragrafo = str_replace($censura, $sostituisci, $intero_par);
+
+echo strlen($new_paragrafo);
+
+
+?>    
+</h1>
 
 </body>
 </html>
